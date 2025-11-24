1724962df1530da6a1be4fafefeb0161e122f5dc
Smit Vora <smitvora10@gmail.com>
2025-11-24 20:38:07 +0530

    migrations added

---
+++ Migrations/20251124150039_otpFields.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyTree.Migrations
+{
+    /// <inheritdoc />
+    public partial class otpFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PersonImage",
+                table: "Person",
+                type: "LONGBLOB",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "longblob",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "OtpVerification",
+                columns: table => new
+                {
+                    OtpVerificationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MobileNumber = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    OtpCode = table.Column<string>(type: "varchar(6)", maxLength: 6, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ExpirationTime = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    IsUsed = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    UsedAt = table.Column<DateTime>(type: "DATETIME", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OtpVerification", x => x.OtpVerificationId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OtpVerification");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PersonImage",
+                table: "Person",
+                type: "longblob",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "LONGBLOB",
+                oldNullable: true);
+        }
+    }
+}
